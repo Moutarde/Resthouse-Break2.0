9a01589457fe2f9f0600609f14c7b02dd9d24c1c
Nicolas Kniebihler <nicolask77@free.fr>
2013-10-19 23:47:30 +0200

    event.txt contains all events to be loaded at the beginning of the game

--- src/model/events/Event.java
+++ src/model/events/Event.java
@@ -2,6 +2,10 @@ package model.events;
 
 import gui.UserInterface;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
@@ -45,16 +49,70 @@ public class Event extends Observable {
         hasBeenTriggered = true;
     }
 
-    public static void createEvents(GameModel model) {
-        Event e = new EnterRoomEvent(Room.getRoom("R_PARK"), model.getPlayer());
-        e.addAction(new Pause());
-        e.addAction(new ShowMessage(new Message(UserInterface.getLang().getString("enterRoom"))));
-        eventList.add(e);
+    public static void createEvents(GameModel model) throws IOException {
+        URL url = Room.class.getResource(eventsDescriptorFilePath);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        assert reader != null;
 
-        Event start = new GameStartedEvent(model);
-        start.addAction(new Pause());
-        start.addAction(new ShowMessage(new Message(UserInterface.getLang().getString("firstMessage"))));
-        eventList.add(start);
+        String line = null;
+
+        while ((line = reader.readLine()) != null) {
+            if (line.length() > 1) {
+                Event e = null;
+
+                String[] tokens = line.split(":");
+                String[] eventTokens = tokens[0].split("\\(");
+                String eventStr = eventTokens[0];
+
+                // ARGUMENTS
+                String arg = null;
+                if (eventTokens.length > 1) {
+                    assert eventTokens[1].endsWith(")") : "The parenthesis of event " + tokens[0] + " is not closed";
+                    arg = eventTokens[1].substring(0, eventTokens[1].length() - 1);
+                }
+
+                // GameStarted
+                if (eventStr.equals("GameStarted")) {
+                    e = new GameStartedEvent(model);
+                }
+                // EnterRoom
+                else if (eventStr.equals("EnterRoom")) {
+                    assert arg != null : "The event " + eventStr + " needs arguments";
+
+                    e = new EnterRoomEvent(Room.getRoom(arg), model.getPlayer());
+                }
+                else {
+                    assert false : "Event " + eventStr + " unknown";
+                }
+
+                assert e != null;
+
+                // ACTIONS
+                String[] actions = tokens[1].split(",");
+
+                for (String str : actions) {
+                    e.addAction(getActionFromString(str));
+                }
+
+                eventList.add(e);
+            }
+        }
+    }
+
+    private static IAction getActionFromString(String str) {
+        if (str.equals("Pause")) {
+            return new Pause();
+        }
+        else if (str.startsWith("ShowMessage(")) {
+            String[] tokens = str.split("\\(");
+            assert tokens[1].endsWith(")") : "The parenthesis of action " + str + " is not closed";
+            String arg = tokens[1].substring(0, tokens[1].length() - 1);
+            return new ShowMessage(new Message(UserInterface.getLang().getString(arg)));
+        }
+        else {
+            assert false : "Action " + str + " doesn't exist";
+            return null;
+        }
     }
 
     public static List<Event> getEventList() {
